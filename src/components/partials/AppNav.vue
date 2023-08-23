045621d57cacea7ea0988550ecd70f090f06f36f
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-08-23 12:50:26 +0200

    correzzione nome voce "contatti"

--- src/components/partials/AppNav.vue
+++ src/components/partials/AppNav.vue
@@ -84,7 +84,7 @@ export default {};
             <router-link
               :to="{ name: 'contacts' }"
               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-300 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >Chi siamo</router-link
+              >Contatti</router-link
             >
           </li>
         </ul>
