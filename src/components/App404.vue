f9bad934508d5d47034ced29bc86637bc1aaf1f9
SimoneRicco <smnrcc0@gmail.com>
2023-08-23 17:42:59 +0200

    add error 404

---
+++ src/components/App404.vue
@@ -0,0 +1,23 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div class="container mx-auto px-4 text-center">
+    <img src="../images/grogu.png" alt="" class="m-auto error404img" />
+    <h1 class="text-5xl">Error 404</h1>
+    <p>Siamo spiacenti ma la pagina non è stata trovata</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 20vh;
+}
+img {
+  width: 200px;
+}
+h1 {
+  margin: 0.5rem 0;
+}
+</style>
