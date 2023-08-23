da22fec186135a73dad6641b32423ab369f6decd
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-08-23 10:46:53 +0200

    AppContacs

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "./components/Home.vue";
+import AppContacts from "./components/partials/AppContacts.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: Home,
+    },
+    {
+      path: "/contacts",
+      name: "contacts",
+      component: AppContacts,
     },
   ],
 });
