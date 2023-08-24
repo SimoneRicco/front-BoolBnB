f80dabcb3c9054ddf378b6f9f3b08fd0ccbbb4bf
LambertoNeri <neri.lamberto91@gmail.com>
2023-08-24 13:01:58 +0200

    create register page and router

--- src/components/partials/AppNav.vue
+++ src/components/partials/AppNav.vue
@@ -34,12 +34,15 @@
         
         </button>
       </router-link>
+      <router-link 
+        :to="{ name: 'register' }">
         <button
           type="button"
           class="text-2xl text-white bg-red-300 hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ml-3 mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Registrati
         </button>
+      </router-link>
         <button
           data-collapse-toggle="navbar-sticky"
           type="button"
