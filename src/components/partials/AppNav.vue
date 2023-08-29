889aa6409c976f70267bccff6446d6e187c1f91b
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-08-29 12:59:07 +0200

    add link to backoffice

--- src/components/partials/AppNav.vue
+++ src/components/partials/AppNav.vue
@@ -19,22 +19,14 @@ export default {
         />
       </a>
       <div class="flex md:order-2">
-        <router-link :to="{ name: 'signup' }">
+        <a href="http://localhost:8000/">
           <button
             type="button"
             class="text-2xl text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Accedi
           </button>
-        </router-link>
-        <router-link :to="{ name: 'register' }">
-          <button
-            type="button"
-            class="text-2xl text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ml-3 mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Registrati
-          </button>
-        </router-link>
+        </a>
         <button
           data-collapse-toggle="navbar-sticky"
           type="button"
