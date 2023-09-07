c49753e42da006357742c3eb90d6e5ef36f44470
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-09-07 18:46:35 +0200

    removed search on utilities

--- src/components/partials/AppSearch.vue
+++ src/components/partials/AppSearch.vue
@@ -337,36 +337,6 @@ export default {
             id="dropdownSearch"
             class="z-99 hidden bg-white rounded-lg shadow w-60 dark:bg-gray-700"
           >
-            <div class="p-3">
-              <label for="input-group-search" class="sr-only">Cerca</label>
-              <div class="relative">
-                <div
-                  class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
-                >
-                  <svg
-                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="text"
-                  id="input-group-search"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Search user"
-                />
-              </div>
-            </div>
             <ul
               class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700 dark:text-gray-200"
               aria-labelledby="dropdownSearchButton"
