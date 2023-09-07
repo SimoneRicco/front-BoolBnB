29be41a899a76515307bbe8aa46e822dada58b26
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-09-07 19:47:37 +0200

    removed bg from form in appshow

--- src/components/partials/AppShow.vue
+++ src/components/partials/AppShow.vue
@@ -624,7 +624,7 @@ export default {
         v-if="!isSending"
       >
         <div
-          class="background-color: rgba(0, 0, 0, 0.6) bg-cover contact bg-[url('https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80')]"
+          
           v-if="!isSending"
         >
         
