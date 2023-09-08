1b1760d7ec42b2742789d6f6025dd2074124f151
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-09-08 11:48:31 +0200

    responsive show

--- src/components/partials/AppShow.vue
+++ src/components/partials/AppShow.vue
@@ -265,9 +265,10 @@ export default {
 
 
 
+    
+      <div class="md:flex">
 
-    <div class="flex">
-      <section class="w-5/12 p-5">
+      <section class="w-12/12 p-5 md:w-6/12">
         <h1 class="text-4xl mb-12">Informazioni sulla struttura:</h1>
         <div class="text-3xl mb-12 flex items-baseline">
           <h2 class="text-blue-800">Numero di camere:</h2>
@@ -318,68 +319,9 @@ export default {
             </svg>
           </div>
         </div>
-        <!-- <div class="text-3xl flex items-baseline">
-          <h2 class="text-blue-800">Punteggio:</h2>
-          <div class="flex items-center ml-5">
-            <svg
-              class="w-6 h-6 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="yellow"
-              viewBox="0 0 22 20"
-            >
-              <path
-                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-              />
-            </svg>
-            <svg
-              class="w-6 h-6 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="yellow"
-              viewBox="0 0 22 20"
-            >
-              <path
-                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-              />
-            </svg>
-            <svg
-              class="w-6 h-6 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="yellow"
-              viewBox="0 0 22 20"
-            >
-              <path
-                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-              />
-            </svg>
-            <svg
-              class="w-6 h-6 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="yellow"
-              viewBox="0 0 22 20"
-            >
-              <path
-                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-              />
-            </svg>
-            <svg
-              class="w-6 h-6 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="yellow"
-              viewBox="0 0 22 20"
-            >
-              <path
-                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-              />
-            </svg>
-          </div>
-        </div> -->
+        
       </section>
-      <section class="w-7/12 mt-5">
+      <section class="w-12/12 p-5 md:w-6/12">
         <h1 class="text-4xl mb-12">Servizi della struttura:</h1>
         <div class="p-5 flex flex-wrap gap-5">
           <span v-for="(utility, index) in apartment.utilities" :key="index">
@@ -479,8 +421,22 @@ export default {
       </section>
     </div>
   </div>
-
-  <MapsVue v-if="coordinates.length > 0" :coordinates="coordinates" />
+  <div class="md:flex">
+    <section class="w-12/12 p-5 md:w-6/12">
+      <div>
+        <h1 class="text-4xl mb-12">Posizione:</h1>
+        <MapsVue v-if="coordinates.length > 0" :coordinates="coordinates" />
+      </div>
+    </section>
+    <section class="w-12/12 p-5 md:w-6/12">
+      <h1 class="text-4xl mb-12">Descrizione:</h1>
+      <p class="text-xl">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptas non animi ipsam dolores rem, quod quo laborum nobis ratione excepturi nihil cupiditate voluptatem ad dignissimos dolorum, doloribus, ipsum delectus.
+        Nesciunt qui odio quos inventore sunt delectus, asperiores similique eveniet modi necessitatibus nihil sequi, numquam voluptas repellat explicabo dolore. Animi ipsa beatae at et consectetur iste neque doloremque fuga facere.
+      </p>
+    </section>
+  </div>
+  
 
   <!-- ******************* contattaci *************************** -->
 
@@ -789,7 +745,6 @@ export default {
 }
 
 .highlighted {
-  height: 100%;
   width: 80%;
 }
 
